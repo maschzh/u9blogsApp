2b2910f1f725ef48b91001a3973abe7b8adac18d
maschzh <maschzh_001@163.com>
2015-03-28 07:36:47 +0800

    make it better

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
 	//url:'mongodb://localhost:27017/u9blog'
-	url:'mongodb://maschzh:4741979qa@ds049130.mongolab.com:49130/u9blog'
+	url:'mongodb://maschzh:4741979qa@ds033617.mongolab.com:33617/u9blogsapp'
 };
